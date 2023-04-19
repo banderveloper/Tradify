9cd3fbe0864d2285aee305e49d906ae29dc0a203
Lyakabynka <dima17741@gmail.com>
2023-04-19 15:58:18 +0200

    refactor(identity): make errors sealed

--- src/Tradify.Backend/Tradify.Identity/Tradify.Identity.Application/Errors/ExpectedError.cs
+++ src/Tradify.Backend/Tradify.Identity/Tradify.Identity.Application/Errors/ExpectedError.cs
@@ -3,7 +3,7 @@ using Tradify.Identity.Application.Errors.Common;
 
 namespace Tradify.Identity.Application.Errors;
 
-public class ExpectedError : Error
+public sealed class ExpectedError : Error
 {
     public ExpectedError(string message, ErrorCode errorCode) 
         : base(HttpStatusCode.OK, message,errorCode) {}
--- src/Tradify.Backend/Tradify.Identity/Tradify.Identity.Application/Errors/NotAcceptableError.cs
+++ src/Tradify.Backend/Tradify.Identity/Tradify.Identity.Application/Errors/NotAcceptableError.cs
@@ -3,7 +3,7 @@ using Tradify.Identity.Application.Errors.Common;
 
 namespace Tradify.Identity.Application.Errors;
 
-public class NotAcceptableError : Error
+public sealed class NotAcceptableError : Error
 {
     public NotAcceptableError(string message, ErrorCode errorCode)
         : base(HttpStatusCode.NotAcceptable,message, errorCode) {}
