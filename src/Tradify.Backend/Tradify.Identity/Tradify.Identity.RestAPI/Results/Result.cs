c611d32c92e492fd0dc49d6f09cd025b0812bf97
Lyakabynka <dima17741@gmail.com>
2023-04-19 13:34:28 +0200

    feat(identity): add custom actionresult

---
+++ src/Tradify.Backend/Tradify.Identity/Tradify.Identity.RestAPI/Results/Result.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Tradify.Identity.Application.Errors;
+
+namespace Tradify.Identity.RestAPI.Results;
+
+public class Result : ActionResult
+{
+    public object? Data { get; set; }
+    public Error? Error { get; set; }
+    
+    public override async Task ExecuteResultAsync(ActionContext context)
+    {
+        var response = context.HttpContext.Response;
+
+        response.ContentType = "application/json";
+        response.StatusCode = Error == null ? (int)HttpStatusCode.OK : (int)Error.StatusCode;
+
+        await response.WriteAsJsonAsync(this);
+    }
+}
